01647f8020b9ffd45fed74b075be3675fb356e8d
Ujjwal Chowdhury <90385123+U77w41@users.noreply.github.com>
2022-09-02 21:57:01 +0530

    Updated the templete.py file

--- templete.py
+++ templete.py
@@ -3,27 +3,17 @@ import os
 
 # Directories needed
 dirs = [
-    os.path.join("data", "raw"),
-    os.path.join("data","processed"),
-    "notebooks",
-    "saved_models",
-    "src"
+    os.path.join("1 Week Preparation Kit", "Mock Test"),
+    os.path.join("1 Month Preparation Kit","Mock Test"),
+    "Python",
+    "Alogrithms",
+    "Data Structures",
+    "Mathematics",
+    "SQL"
 ]
 
 # Create the directories and write a .gitkeep file inside each directory (we cannot commit an empty directory in git)
 for dir in dirs:
     os.makedirs(dir , exist_ok=True)
     with open(os.path.join(dir , ".gitkeep"),"w") as f: # writing "w" for specifing that we want to write the file also
-        pass
-        
-
-files = [
-    'params.yaml',
-    '.gitignore', # Here we push all the files which we dont want to push into git
-    os.path.join('src' , '__init__.py' )
-
-]
-
-for file_ in files:
-    with open(file_, "w") as f:
         pass
