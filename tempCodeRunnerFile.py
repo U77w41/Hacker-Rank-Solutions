2aec76250ad47098c87c7d8c1dcfdd3d258e2e71
Ujjwal Chowdhury <90385123+U77w41@users.noreply.github.com>
2022-09-02 21:58:36 +0530

    Ran  templete.py

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+### This is to create the the folders required
+import os
+
+# Directories needed
+dirs = [
+    os.path.join("1 Week Preparation Kit", "Mock Test"),
+    os.path.join("1 Month Preparation Kit","Mock Test"),
+    "Python",
+    "Alogrithms",
+    "Data Structures",
+    "Mathematics",
+    "SQL"
+]
+
+# Create the directories and write a .gitkeep file inside each directory (we cannot commit an empty directory in git)
+for dir in dirs:
+    os.makedirs(dir , exist_ok=True)
+    with open(os.path.join(dir , ".gitkeep"),"w") as f: # writing "w" for specifing that we want to write the file also
+        pass
