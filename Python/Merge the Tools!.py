6bbd7d0bd56eb8e380709000446a76933c61c9f1
Ujjwal Chowdhury <90385123+U77w41@users.noreply.github.com>
2022-09-05 13:08:53 +0530

    added Solution

--- Python/Merge the Tools!.py
+++ Python/Merge the Tools!.py
@@ -1,1 +1,18 @@
 
+def merge_the_tools(string, k):
+    # your code goes here
+    
+    substring=[]
+    
+    for i,l in enumerate(string):
+        if l not in substring:
+            substring.append(l)
+        if i%k==k-1:
+            print(''.join(substring))
+            substring=[]
+
+            
+            
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
