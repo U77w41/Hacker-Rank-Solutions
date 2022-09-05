f23e75a4e16346565b61599ac2fa1797120c24b0
Ujjwal Chowdhury <90385123+U77w41@users.noreply.github.com>
2022-09-05 13:21:43 +0530

    Added Solution

---
+++ Python/Validating Postal Codes.py
@@ -0,0 +1,19 @@
+"""
+Reference: https://www.chase2learn.com/validating-postal-codes-in-python-hackerrank-solution/
+"""
+
+
+
+
+# Validating Postal Codes in Python - Hacker Rank Solution START
+regex_integer_in_range = r"^[1-9][\d]{5}$"    # Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"    # Do not delete 'r'.
+# Validating Postal Codes in Python - Hacker Rank Solution END
+
+
+
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
